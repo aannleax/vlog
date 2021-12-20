b0020028e42fff5e7ca3b2a675fa92cd2b644c64
Alex Ivliev <alex@ivliev.de>
2021-12-20 09:00:22 +0100

    Loading rule set

--- include/vlog/reliances/experiments.h
+++ include/vlog/reliances/experiments.h
@@ -1,6 +1,9 @@
 #include <vlog/reliances/reliances.h>
+#include <vlog/concepts.h>
+#include <vlog/edbconf.h>
+#include <vlog/edb.h>
 
 #include <iostream>
 #include <string>
 
-void experimentCoreStratified(const std::string &rulePath, bool pieceDecomposition);
+void experimentCoreStratified(const std::string &rulesPath, bool pieceDecomposition);
--- src/vlog/reliances/experiments.cpp
+++ src/vlog/reliances/experiments.cpp
@@ -1,8 +1,18 @@
 #include <vlog/reliances/experiments.h>
 
-void experimentCoreStratified(const std::string &rulePath, bool pieceDecomposition)
+void experimentCoreStratified(const std::string &rulesPath, bool pieceDecomposition)
 {
-    std::cout << "Launches coreStratified experiment with parameters " << '\n';
-    std::cout << "\t" << rulePath << '\n';
+    std::cout << "Launched coreStratified experiment with parameters " << '\n';
+    std::cout << "\t" << rulesPath << '\n';
     std::cout << "\t" << ((pieceDecomposition) ? "true" : "false") << std::endl;
+
+    EDBConf emptyConf("", false);
+    EDBLayer edbLayer(emptyConf, false);
+
+    Program program(&edbLayer);
+    std::string errorString = program.readFromFile(rulesPath, false);
+    if (!errorString.empty()) {
+        LOG(ERRORL) << errorString;
+        return;
+    }
 }
